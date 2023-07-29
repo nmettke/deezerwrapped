64a206e826915b27452e2a617901b324bd0ddc53
nmettke <mettke@umich.edu>
2023-07-29 00:43:20 -0400

    Testing user history limits

--- music.py
+++ music.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect
 import requests
 from dotenv import dotenv_values
+import json
+import time
 
 
 app = Flask(__name__)
@@ -19,24 +21,29 @@ def default():
 
 @app.route('/deezer/login', methods=['GET'])
 def deezer_login():
-    # retrieve the authorization code given in the url
     code = request.args.get('code')
 
-    # request the access token
     url = (f'https://connect.deezer.com/oauth/access_token.php?app_id={DEEZER_APP_ID}'
            f'&secret={DEEZER_APP_SECRET}&code={code}&output=json')
     response = requests.get(url)
     
-    # If it's not a good code we will get this error
     if response.text == 'wrong code':
         return 'wrong code'
     
-    # We have our access token
     access_token = response.json()['access_token']
     
-    url = f'https://api.deezer.com/user/me/history?access_token={access_token}'
-    response = requests.get(url)
-    history = response.json()
+    history = []
+    for i in range(20):
+        url = f'https://api.deezer.com/user/me/history?access_token={access_token}&index={i * 50}'
+        response = requests.get(url)
+        data = response.json()
+        if 'data' in data:
+            print("Received ", len(data['data']), " items")
+            history += data['data']
+        time.sleep(.5)
+    with open("history_json", 'w') as f:
+        json.dump(history, f)
+    print("length of history: ", len(history))
     return history
 
 if __name__ == '__main__':
