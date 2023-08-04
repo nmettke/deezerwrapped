0f0ebd1cb3121e9b622bf9ee74d3594f29f91e4e
nmettke <mettke@umich.edu>
2023-08-03 20:15:21 -0400

    Jinja2

--- music.py
+++ music.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import requests
 from dotenv import dotenv_values
 import json
 import time
-
+import jinja2
 
 class MyData:
     def __init__(self):
@@ -11,16 +11,34 @@ class MyData:
         self.access_token = ""
         self.artist_freq = {}
         self.genre_freq = {}
+        self.name = ""
 
     def process_results(self):
+        x1 = time.time()
         for song in self.history:
-            name = song['artist']['name']
-            if name in self.artist_freq:
-                self.artist_freq[name] += 1
+            artist_id = song['artist']['id']
+            album_id = song['album']['id']
+            if artist_id in self.artist_freq:
+                self.artist_freq[artist_id] += 1
             else:
-                self.artist_freq[name] = 1
-        print(self.artist_freq)
-        return self.artist_freq
+                self.artist_freq[artist_id] = 1
+
+            url = f'https://api.deezer.com/album/{album_id}?access_token={data.access_token}'
+            res_data = requests.get(url).json()
+            if 'genres' in res_data and 'data' in res_data['genres']:
+                for genre in res_data['genres']['data']:
+                    genre_name = genre['id']
+                    if genre_name in self.genre_freq:
+                        self.genre_freq[genre_name] += 1
+                    else:
+                        self.genre_freq[genre_name] = 1
+        x2 = time.time()
+        print("time taken to process results: ", x2 - x1)
+    
+    def fetch_results(self):
+        if len(self.artist_freq) == 0 or len(self.genre_freq) == 0:
+            self.process_results()
+        return self.artist_freq, self.genre_freq
 
 
 app = Flask(__name__)
@@ -69,7 +87,11 @@ def deezer_login():
 
 @app.route('/results')
 def results():
-    return data.process_results()
+    artist_freq, genre_freq = data.fetch_results()
+    url = f'https://api.deezer.com/user/me/name?access_token={data.access_token}'
+    res_data = requests.get(url).json()
+    name = res_data['name']
+    return render_template('results.html', artist_freq=artist_freq, genre_freq=genre_freq, name=name)
 
 if __name__ == '__main__':
     app.run()
